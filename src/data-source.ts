f1df3b442ccf37d76f13121e08b0d75cb14d00f2
leonarjin <izumi.jin@live.com>
2024-04-15 18:35:55 +0100

    variaveis

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,24 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
-import { Tasks } from "./entity/Tasks"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Tasks } from "./entity/Tasks";
+import "dotenv/config";
 
+const getDBConfig = (json: any) => {
+    const newObj = {};
+
+    for (const prop in json) {
+        if (!prop.startsWith("DB_")) {
+            continue;
+        }
+        const newProp = prop.substring(prop.indexOf("_") + 1).toLowerCase();
+        newObj[newProp] = json[prop];
+    }
+
+    return newObj;
+};
+
+const config = getDBConfig(process.env) as DataSourceOptions;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -17,3 +33,4 @@ export const AppDataSource = new DataSource({
     migrations: [],
     subscribers: [],
 })
+
